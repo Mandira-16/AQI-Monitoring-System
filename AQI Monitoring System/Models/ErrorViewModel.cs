149b020640a23428ca1d7e44a680e2e09b03a5be
Mandira-16 <10952854@students.plymouth.ac.uk>
2025-03-23 09:39:36 +0530

    Add project files.

---
+++ AQI Monitoring System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AQI_Monitoring_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
