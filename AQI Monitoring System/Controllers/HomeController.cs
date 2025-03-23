68abe96805813ad5460c9d7e618a0f9894846a3e
Mandira-16 <10952854@students.plymouth.ac.uk>
2025-03-23 11:39:30 +0530

    Added the login view path

--- AQI Monitoring System/Controllers/HomeController.cs
+++ AQI Monitoring System/Controllers/HomeController.cs
@@ -23,6 +23,33 @@ namespace AQI_Monitoring_System.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /Home/Login
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Here you would add your authentication logic
+                // For example:
+                // if (AuthenticateUser(model.Username, model.Password))
+                // {
+                //     // Set authentication cookie or token
+                //     return RedirectToAction("Index");
+                // }
+
+                // For now, just redirect to home page after login attempt
+                return RedirectToAction("Index");
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
