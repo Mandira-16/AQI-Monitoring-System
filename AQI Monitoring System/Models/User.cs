74453a5ef1efdc204a1480bbc9830d6e36afd479
Mandira-16 <10952854@students.plymouth.ac.uk>
2025-03-25 20:15:50 +0530

    Users Model

---
+++ AQI Monitoring System/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace AQI_Monitoring_System.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        // Other user properties
+    }
+}
