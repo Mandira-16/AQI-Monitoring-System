7acc8270e637aa3b7b3fef14535b8ccfbe7b9d8e
Mandira-16 <10952854@students.plymouth.ac.uk>
2025-03-25 20:16:30 +0530

    IUserService

---
+++ AQI Monitoring System/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using AQI_Monitoring_System.Models;
+
+namespace AQI_Monitoring_System.Services
+{
+    public interface IUserService
+    {
+        User GetUserByUsername(string username);
+        bool VerifyPassword(User user, string password);
+    }
+}
