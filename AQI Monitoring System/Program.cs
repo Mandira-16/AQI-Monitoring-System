9c1a08d4c13a9b56516240f6f83951b84f94bd91
Mandira-16 <10952854@students.plymouth.ac.uk>
2025-03-25 20:18:04 +0530

    Added the Service

--- AQI Monitoring System/Program.cs
+++ AQI Monitoring System/Program.cs
@@ -1,7 +1,11 @@
+using AQI_Monitoring_System.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// Added this line 
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
