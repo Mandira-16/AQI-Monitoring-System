2b907de07a1e0d6ad0ceb8af2eca45c5db108596
Mandira-16 <10952854@students.plymouth.ac.uk>
2025-03-27 12:13:02 +0530

    Configure ApplicationDbContext to use MySQL with EF Core, define Users DbSet, and map User entity to 'User' table.

---
+++ AQI Monitoring System/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AQI_Monitoring_System.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AQI_Monitoring_System.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Map the User entity to a table named "User"
+            modelBuilder.Entity<User>().ToTable("User");
+        }
+    }
+}
