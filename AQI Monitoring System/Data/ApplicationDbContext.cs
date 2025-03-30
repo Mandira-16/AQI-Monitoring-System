2ea0219c3f503d73c7c234ca0a950c3c68453b6b
Mandira-16 <10952854@students.plymouth.ac.uk>
2025-03-30 23:07:06 +0530

    added AqiReadings

--- AQI Monitoring System/Data/ApplicationDbContext.cs
+++ AQI Monitoring System/Data/ApplicationDbContext.cs
@@ -11,10 +11,16 @@ namespace AQI_Monitoring_System.Data
         }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Sensor> Sensors { get; set; }
+        public DbSet<AqiReading> AqiReadings { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // Map the User entity to a table named "User"
             modelBuilder.Entity<User>().ToTable("User");
+            // Map the Sensor entity to a table named "Sensors"
+            modelBuilder.Entity<Sensor>().ToTable("Sensors");
+            modelBuilder.Entity<AqiReading>().ToTable("AqiReadings");
         }
     }
 }
