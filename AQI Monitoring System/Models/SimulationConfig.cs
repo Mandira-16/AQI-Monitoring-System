bd85577714ed20498e84e9419afd1e4312556b75
Mandira-16 <10952854@students.plymouth.ac.uk>
2025-03-31 15:14:59 +0530

    Add SimulationConfig model with Id, FrequencyMinutes, BaselineAqi, and VariationRange properties

--- AQI Monitoring System/Models/SimulationConfig.cs
+++ AQI Monitoring System/Models/SimulationConfig.cs
@@ -4,8 +4,8 @@ namespace AQI_Monitoring_System.Models
     public class SimulationConfig
     {
         public int Id { get; set; }
-        public int FrequencyMinutes { get; set; } // Frequency of data generation in minutes (e.g., 5, 10, 15)
-        public int BaselineAqi { get; set; }      // Starting AQI value
-        public int VariationRange { get; set; }   // Maximum deviation from baseline (e.g., ±20)
+        public int FrequencyMinutes { get; set; }
+        public int BaselineAqi { get; set; }
+        public int VariationRange { get; set; }
     }
 }
