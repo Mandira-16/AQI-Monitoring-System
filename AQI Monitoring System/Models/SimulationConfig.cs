7bd494a4935fa1af15728b6436403bf3e2c7fd4c
Mandira-16 <10952854@students.plymouth.ac.uk>
2025-04-12 19:28:34 +0530

    Update the SimulationConfig model for other readings

--- AQI Monitoring System/Models/SimulationConfig.cs
+++ AQI Monitoring System/Models/SimulationConfig.cs
@@ -7,5 +7,17 @@ namespace AQI_Monitoring_System.Models
         public int FrequencyMinutes { get; set; }
         public int BaselineAqi { get; set; }
         public int VariationRange { get; set; }
-    }
+		public double BaselinePm25 { get; set; }
+		public double VariationPm25 { get; set; }
+		public double BaselinePm10 { get; set; }
+		public double VariationPm10 { get; set; }
+		public double BaselineO3 { get; set; }
+		public double VariationO3 { get; set; }
+		public double BaselineNo2 { get; set; }
+		public double VariationNo2 { get; set; }
+		public double BaselineSo2 { get; set; }
+		public double VariationSo2 { get; set; }
+		public double BaselineCo { get; set; }
+		public double VariationCo { get; set; }
+	}
 }
