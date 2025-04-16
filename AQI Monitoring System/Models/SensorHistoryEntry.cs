0d924c4962abdae7afea9dff245075fbff417dd1
Mandira-16 <10952854@students.plymouth.ac.uk>
2025-04-16 17:43:51 +0530

    Add SensorHistoryEntry model for historical data

---
+++ AQI Monitoring System/Models/SensorHistoryEntry.cs
@@ -0,0 +1,13 @@
+﻿namespace AQI_Monitoring_System.Models
+{
+    public class SensorHistoryEntry
+    {
+        public string RecordedAt { get; set; }
+        public double? Pm25 { get; set; }
+        public double? Pm10 { get; set; }
+        public double? O3 { get; set; }
+        public double? No2 { get; set; }
+        public double? So2 { get; set; }
+        public double? Co { get; set; }
+    }
+}
